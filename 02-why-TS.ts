16cae09d3f8a21bd1f0adc6f8f683d5c32ce5a43
emmeiwhite <emmeiwhite@gmail.com>
2025-05-20 15:17:55 +0530

    ts-1.4 TS catches undeclared variable number immediately

--- 02-why-TS.ts
+++ 02-why-TS.ts
@@ -1,7 +1,15 @@
 let value: number = 2
 
-function calculate(num1: number, num2: number) {
+function sumTS(num1: number, num2: number) {
   return num1 + num2
 }
 
 console.log(sum(value, 5))
+
+function calculateSumTS(numbers) {
+  return number.reduce((acc, item) => {
+    return acc + item
+  }, 0)
+}
+
+calculateSumTS([2, 3, 4, 5])
