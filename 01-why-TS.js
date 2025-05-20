155ee18278597c80718ae0ef5245cc4a1eedb505
emmeiwhite <emmeiwhite@gmail.com>
2025-05-20 15:14:45 +0530

    ts-1.3 there is a bug in example-2, we have used number instead of numbers

--- 01-why-TS.js
+++ 01-why-TS.js
@@ -1,6 +1,16 @@
+/** Example-1: Catching bugs */
 let num1 = '2'
 function sum(num1, num2) {
   return num1 + num2
 }
 
 console.log(sum(num1, 5))
+
+/** Example-2: A bug un-noticed before running the code. */
+function calculateSum(numbers) {
+  return number.reduce((acc, item) => {
+    return acc + item
+  }, 0)
+}
+
+calculateSum([2, 3, 4, 5])
---
+++ 02-why-TS.ts
@@ -0,0 +1,7 @@
+let value: number = 2
+
+function calculate(num1: number, num2: number) {
+  return num1 + num2
+}
+
+console.log(sum(value, 5))
