69269732cdd60a6de373836ba7dad765165052be
emmeiwhite <emmeiwhite@gmail.com>
2025-05-20 15:35:00 +0530

    ts-1.6 auto-completion in action without ts

--- why-typescript/02-why-TS.ts
+++ why-typescript/02-why-TS.ts
@@ -13,3 +13,12 @@ function calculateSumTS(numbers) {
 }
 
 calculateSumTS([2, 3, 4, 5])
+
+function findSum(arr: Array<number>) {
+  return arr.reduce((acc: number, num: number) => {
+    return (acc = acc + num)
+  }, 0)
+}
+
+const result = findSum([1, 2, 3, 4])
+console.log(result)
