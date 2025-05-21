957877c29363e41a6d67557919c1918be2a732d6
emmeiwhite <emmeiwhite@gmail.com>
2025-05-21 18:42:20 +0530

    ts-2.0 assertion example

--- app.ts
+++ app.ts
@@ -16,3 +16,21 @@ function getOldest(people: HasAge[]): HasAge {
 const people: HasAge[] = [{ age: 30 }, { age: 33 }, { age: 25 }]
 
 getOldest(people)
+
+interface Player {
+  name: string
+  age: number
+}
+
+let players: Player[] = [
+  { name: 'john', age: 33 },
+  { name: 'mike', age: 20 },
+  { name: 'harry', age: 43 }
+]
+
+getOldest(players) // The getOldest function will not throw any warning, since age property is present in Player interface but we cannot access name property with this approach, For example if we try to do getOldest(players). (we only get auto-suggestion for age )
+
+// We can do ASSERTION here but that is not considered a good practice
+
+const player2 = getOldest(players) as Player
+player2.name // we can access the name now with assertion
