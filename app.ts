618622fc45a9d756400cbec3778382200c6f56c4
emmeiwhite <emmeiwhite@gmail.com>
2025-05-21 18:36:39 +0530

    ts-2.0 formulating an example of generics

--- app.ts
+++ app.ts
@@ -1,48 +1,18 @@
-import { log } from 'console'
-
-/** --- Generics Example: Learning
- * ASSUME WE HAVE GOT A LOGGER FUNCTION WHICH LOGS DIFFERENT TYPES
- * --- */
-function logString(arg: string) {
-  console.log(arg)
-  return arg
-}
-
-logString('3')
-
-function logNumber(arg: number) {
-  console.log(arg)
-  return arg
-}
-
-logNumber(34)
-
-function logArr(arg: any[]) {
-  console.log(arg)
-  return arg
-}
-
-logArr([2, 3])
-
-/**
- * We are repeating same lines of code in all the functions:
- *   
-     console.log(arg)
-     return arg
-
-     which is considered a bad practice.
-
-     Now, we can go with any keyword but that will be a bad option.
-
-     Instead we make use of GENERIC TYPE - which can take any type of arguments in an efficient way
+/** GENERICS EXAMPLE:
+ *  A function which takes array of objects of certain contract (types) and returns on object of that type.
  */
-
-/** --- Writing my first Generic Line of code and very excited about this --- */
-function logAnything<T>(arg: T): T {
-  console.log(arg)
-  return arg
+interface HasAge {
+  age: number
 }
 
-console.log(logAnything([2, 3, 4]))
-console.log(logAnything({ age: 33 }))
-console.log(logAnything('Keep moving forward'))
+function getOldest(people: HasAge[]): HasAge {
+  // DSA: Find oldest in an array. Sort it and return first element
+
+  return people.sort((a, b) => {
+    return b.age - a.age
+  })[0]
+}
+
+const people: HasAge[] = [{ age: 30 }, { age: 33 }, { age: 25 }]
+
+getOldest(people)
