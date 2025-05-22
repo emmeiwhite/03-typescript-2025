d75d01bbc3919144cc7e5a81e14f331f7c87dbc5
emmeiwhite <emmeiwhite@gmail.com>
2025-05-22 11:18:52 +0530

    ts-2.3 generics fetch example 3 (LIP)

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-/** 3.  */
+/** --- 3. GENERICS FETCH EXAMPLE --- */
+
+interface IPost {
+  title: string
+  id: number
+  description: string
+}
+
+async function fetchPosts(path: string): Promise<IPost[]> {
+  const response = await fetch(`http://example.com${path}`)
+  return response.json()
+}
+
+;(async () => {
+  const posts = await fetchPosts('/posts')
+  posts[0]
+})()
